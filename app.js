54eb72cd6cca67b0bd1401e1c9cdc8f3489e9641
ahmed.tawfik <ahmed.tawfik@itworx.com>
2022-09-17 00:09:42 +0200

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const observer = new IntersectionObserver((entries)=>{
+    console.log(entries);
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>{
+    console.log(el)
+    observer.observe(el)
+})
+
